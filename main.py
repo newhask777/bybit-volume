46a1fdd63588b4bea305e17450f878f07f03e127
newhask777 <newhaskellisp@gmail.com>
2024-06-04 15:09:40 +0300

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+from pybit.unified_trading import HTTP
+from Crypto.Cipher import AES
+import json
+
+session = HTTP()
+
+req = session.get_kline(category="linear", symbol="BTCUSDT", interval=5,)
+print(req)
+
+with open('klines.json', 'w', encoding='utf-8') as f:
+    json.dump(req, f, indent=4, ensure_ascii=False)
---
+++ tickers.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+bybit = requests.get("https://api-testnet.bybit.com//v5/market/tickers?category=linear")
+e = bybit.json()['result']['list']
+
+print(e)
+
+# with open('tickers.json', 'w', encoding='utf-8') as f:
+#     json.dump(e, f, indent=4, ensure_ascii=False)
+
+symbols = []
+
+for pair in e:
+    symbol = pair['symbol']
+    symbols.append(symbol)
+    print (symbol)
+
+
+    with open('symbols.json', 'w', encoding='utf-8') as f:
+        json.dump(symbols, f, indent=4, ensure_ascii=False)
